80640aaa34cdb904bdf36873dc7b9adef5625829
Serafim <serafimkremnev1008@gmail.com>
2023-03-01 17:43:59 +0300

    edit lay

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import styles from './Header.module.css'
+import {Box, Typography} from "@mui/material";
 
 const Header = () => {
 
     return (
-        <div>
-            <h1 className={styles.logo}>CodeBench</h1>
-        </div>
+        <Box display={'flex'}>
+            <Typography fontWeight={300} fontSize={'25px'} color={'white'}>
+                Code
+            </Typography>
+            <Typography fontWeight={600} fontSize={'25px'} color={'primary'}>
+                Bench
+            </Typography>
+        </Box>
     );
 };
 
